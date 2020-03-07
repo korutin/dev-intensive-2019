0b3d4e070fcbf96648bb844fd1977608ee5a9881
korutin <ks.emelianov@yandex.ru>
2020-03-07 09:50:52 +0300

    hometask_2 complete

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,7 +1,7 @@
 package ru.skillbranch.devintensive.extensions
 
-fun String.truncate(nSym:Int?) : String {
-    var S : String = ""
+fun String.truncate(nSym:Int?=null) : String {
+    var S : String = this
     nSym?.let {S =this.substring(0, if (this.length>nSym) nSym else this.length)+"..." }
     return S
 }
